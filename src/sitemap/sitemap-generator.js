74437f50665000ef2763d535902b26ddea7163eb
marwal1987 <144460382+marwal1987@users.noreply.github.com>
2024-09-18 08:33:01 +0200

    Updated sitemap script and routes

--- src/sitemap/sitemap-generator.js
+++ src/sitemap/sitemap-generator.js
@@ -1,13 +1,41 @@
-import { SitemapStream, streamToPromise } from 'sitemap';
-import { createWriteStream } from 'fs';
-import routes from './sitemap-routes.js'; // Dina rutter
+import { SitemapStream, streamToPromise } from "sitemap";
+import { createWriteStream } from "fs";
+import routes from "./sitemap-routes.js"; // Rutter
+import axios from "axios";
 
-const sitemap = new SitemapStream({ hostname: 'https://example.com' });
-const writeStream = createWriteStream('./public/sitemap.xml');
+async function generateSitemap() {
+  const response = await axios.get("https://fakestoreapi.com/products");
+  const products = response.data;
 
-routes.forEach(route => {
-  sitemap.write({ url: route.path, changefreq: 'daily', priority: 0.8 });
-});
+  const sitemap = new SitemapStream({ hostname: "http://localhost" });
+  const writeStream = createWriteStream("./public/sitemap.xml");
 
-sitemap.end();
-streamToPromise(sitemap.pipe(writeStream));
+  sitemap.pipe(writeStream);
+
+  // Statiska sidor - Skapa manuellt
+  // sitemap.write({ url: '/', changefreq: 'daily', priority: 1.0 });
+  // sitemap.write({ url: '/cart', changefreq: 'daily', priority: 0.8 });
+
+  // Skapa sidor med forEach
+  routes.forEach((route) => {
+    sitemap.write({
+      url: route.path,
+      changefreq: route.changefreq,
+      priority: route.priority,
+    });
+  });
+
+  // Dynamiska produkt-URL:er
+  products.forEach((product) => {
+    sitemap.write({
+      url: `/product/${product.id}`,
+      changefreq: "daily",
+      priority: 0.8,
+    });
+  });
+
+  sitemap.end();
+  await streamToPromise(sitemap);
+}
+
+generateSitemap();
--- src/sitemap/sitemap-routes.js
+++ src/sitemap/sitemap-routes.js
@@ -1,7 +1,7 @@
+// OBS! Här hanteras statiska rutter - EJ dynamiska. Dynamiska rutter hanteras i sitemap-generator.js
 const routes = [
-  { path: "/" },
-  { path: "/product/:id" },
-  { path: "/cart" }
+  { path: "/", priority: 1.0, changefreq: "daily" },
+  { path: "/about", priority: 0.9, changefreq: "monthly" },
 ];
 
 export default routes;
